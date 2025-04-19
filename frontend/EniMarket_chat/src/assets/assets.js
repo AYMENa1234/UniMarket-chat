09ff3a6a431af14d4c9cd6c411e85c9701bc073b
amin <aminbouallegui0@gmail.com>
2025-04-20 00:47:58 +0200

    commit

--- frontend/EniMarket_chat/src/assets/assets.js
+++ frontend/EniMarket_chat/src/assets/assets.js
@@ -2,10 +2,14 @@
 import icon from './icon.png';
 import search from './search.png';
 import menu from './menu.png';
+import help from './help.png';
 import profile from './profile.webp';
+import green from './green.png';
 export const assets = {
     search,
     profile,
+    help,
+    green,
     icon,
     menu,
 }
--- frontend/EniMarket_chat/src/components/Chat/Chat.jsx
+++ frontend/EniMarket_chat/src/components/Chat/Chat.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import './Chat.css'
+import React from 'react';
+import './Chat.css';
+import { assets } from '../../assets/assets';
+
 const Chat = () => {
   return (
     <div className='chat'>
-        <h1>chat</h1>
+      <div className="chat_user">
+        <div style={{ position: 'relative' }}>
+          <img src={assets.profile} alt="User Profile" />
+          <div className="status"></div>
+        </div>
+        <div>
+          <p>Amin Bouallegui</p>
+          <span>En ligne</span>
+        </div>
+        <div className="chatuserh">
+          <img src={assets.help} alt="Help Icon" />
+        </div>
+      </div>
+      <div className="chat_message">
+        <input type="text " placeholder='send a message' />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
